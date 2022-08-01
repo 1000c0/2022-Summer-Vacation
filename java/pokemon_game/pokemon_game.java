af435d56da18eb2796ea61090deedf2ac1c3416d
1000c0 <siyeongcheon@gmail.com>
2022-08-01 15:06:14 +0900

    some bug fixed and add ending credit-1

--- java/pokemon_game/pokemon_game.java
+++ java/pokemon_game/pokemon_game.java
@@ -1,6 +1,5 @@
 package pokemon_game;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,16 +10,6 @@ public class pokemon_game {
 	public static void main(String[] args) {
 		String[] all_mon = { "아르세우스", "레쿠자", "뮤", "칠색조", "나인테일", "그란돈", "레시라무", "히드런", "얼음귀신", "눈설왕", "펄기아", "큐레무",
 				"갸라도스", "루기아", "가이오가", "스이쿤", "파이어", "프리져", "썬더" };
-
-		String[][] all_skill = { { "심판의뭉치", "신속", "화염방사", "용성군" }, { "화룡점정", "신속", "냉동빔", "화염방사" },
-				{ "사이코키네시스", "파동탄", "막치기", "냉동펀치" }, { "성스러운불꽃", "불대문자", "바람 일으키기", "원시의힘" },
-				{ "불대문자", "화염방사", "불꽃세례", "전광석화" }, { "단에의칼", "불대문자", "원시의 힘", "솔라빔" },
-				{ "푸른불꽃", "크로스플레임", "역린", "용의숨결" }, { "대지의힘", "스톤에지", "마그마스톤", "아이언헤드" },
-				{ "눈보라", "깨물어부수기", "프리즈드라이", "얼음숨결" }, { "눈보라", "우드헤머", "냉동펀치", "나뭇잎" },
-				{ "공간절단", "하이드로펌프", "드래곤크루", "용의숨결" }, { "프리즈드라이", "냉동빔", "크로스썬더", "크로스플레임" },
-				{ "파괴광선", "하이드로펌프", "난동부리기", "물기" }, { "에어로블라스트", "불새", "미래예지", "드래곤다이브" },
-				{ "근원의파동", "해수스파우팅", "하이드로펌프", "아쿠아테일" }, { "눈보라", "하이드로펌프", "깨물어부수기", "신속" },
-				{ "불꽃세례", "불태우기", "폭풍", "불새" }, { "얼음뭉치", "프리즈드라이", "냉동빔", "폭풍" }, { "전기쇼크", "원시의힘", "번개", "방전" } };
 
 		ArrayList<String> skill_dic = new ArrayList<>(Arrays.asList("심판의뭉치", "신속", "화염방사", "용성군", "화룡점정", "신속", "냉동빔",
 				"화염방사", "사이코키네시스", "파동탄", "막치기", "냉동펀치", "성스러운불꽃", "불대문자", "바람 일으키기", "원시의힘", "불대문자", "화염방사", "불꽃세례",
@@ -35,28 +24,17 @@ public class pokemon_game {
 				75, 60, 45, 30, 50, 70, 60, 50, 55, 75, 55, 45, 55, 55, 40, 40, 20, 30, 55, 70, 20, 35, 45, 55, 20, 30,
 				55, 40 };
 
-		int[] all_hp = { 240, 210, 200, 212, 146, 200, 200, 182, 160, 180, 180, 250, 190, 212, 200, 200 };
-
-		int[][] all_skill_power = { { 50, 40, 45, 65 }, { 60, 40, 45, 45 }, { 45, 40, 20, 40 }, { 50, 55, 20, 30 },
-				{ 55, 45, 20, 20 }, { 60, 55, 30, 60 }, { 65, 50, 60, 30 }, { 45, 50, 50, 40 }, { 55, 40, 35, 30 },
-				{ 55, 60, 40, 20 }, { 50, 55, 40, 30 }, { 35, 45, 50, 50 }, { 75, 60, 45, 30 }, { 50, 70, 60, 50 },
-				{ 55, 75, 55, 45 }, { 55, 55, 40, 40 }, { 20, 30, 55, 70 }, { 20, 35, 45, 55 }, { 20, 30, 55, 40 } };
-
 		ArrayList<String> inven_mon = new ArrayList<>();
 		ArrayList<Integer> inven_hp = new ArrayList<>();
 		ArrayList<String> inven_skill = new ArrayList<>();
 
 		String[] start = { "파이어", "프리져", "썬더" };
-		int[] start_hp = { 180, 180, 180 };
 
 		ArrayList<String> fire_skill = new ArrayList<>(Arrays.asList("불꽃세례", "불태우기", "폭풍", "불새"));
-		int[] fire_skillp = { 20, 30, 55, 70 };
 
 		ArrayList<String> free_skill = new ArrayList<>(Arrays.asList("얼음뭉치", "프리즈드라이", "냉동빔", "폭풍"));
-		int[] free_skillp = { 20, 35, 45, 55 };
 
 		ArrayList<String> thunder_skill = new ArrayList<>(Arrays.asList("전기쇼크", "원시의힘", "번개", "방전"));
-		int[] thunder_skillp = { 20, 30, 55, 40 };
 
 		String[] grass = { "아르세우스", "레쿠자", "뮤", "칠색조" };
 		int[] grass_hp = { 240, 210, 200, 212 };
@@ -135,6 +113,7 @@ public class pokemon_game {
 		System.out.println(" .\n .\n ");
 		System.out.println(name + ": 여라가지 길이 있군, 어디로 갈까?");
 
+		outerLoop2:
 		while (true) {
 			System.out.println("[초원 / 화산 / 설원 / 바다/ BOSS]");
 			System.out.print("가고 싶은 지역을 입력해주세요: ");
@@ -176,6 +155,10 @@ public class pokemon_game {
 											System.out.println(grass[i] + "를 처치했습니다.");
 											break outerLoop;
 										}
+										if (my_hp <= 0) {
+											System.out.println(inven_mon.get(0) + "가 쓰러졌습니다.");
+											break outerLoop;
+										}
 										System.out.println(grass[i] + "의 " + skill_dic.get((4 * j) + s));
 										break;
 									}
@@ -209,7 +192,7 @@ public class pokemon_game {
 								continue;
 							}
 						} else if (use.equals("2") | use.equals("회복약")) {
-							if(my_hp == inven_hp.get(0)) {
+							if (my_hp == inven_hp.get(0)) {
 								System.out.println("최대 체력입니다.");
 							}
 							if (my_hp < (inven_hp.get(0))) {
@@ -226,16 +209,21 @@ public class pokemon_game {
 							System.out.println("교체할 포켓몬이 없습니다.");
 							continue;
 						} else if (count > 0) {
-							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까?(1,2,3,4,5)");
+							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까? (1~" + inven_mon.size() + ")");
 							int change = sc.nextInt();
-							Collections.swap(inven_mon, 0, change - 1);
-							Collections.swap(inven_hp, 0, change - 1);
-							Collections.swap(inven_skill, 0, 4 * (change - 1));
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
-							my_hp = inven_hp.get(0);
-							continue;
+							if (change == 1) {
+								System.out.println("똑같은 포켓몬으로 교체할 수 없습니다.");
+								continue;
+							} else if (change >= 2) {
+								Collections.swap(inven_mon, 0, change - 1);
+								Collections.swap(inven_hp, 0, change - 1);
+								Collections.swap(inven_skill, 0, 4 * (change - 1));
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
+								my_hp = inven_hp.get(0);
+								continue;
+							}
 						}
 						break;
 					}
@@ -278,6 +266,10 @@ public class pokemon_game {
 											System.out.println(fire[i] + "를 처치했습니다.");
 											break outerLoop;
 										}
+										if (my_hp <= 0) {
+											System.out.println(inven_mon.get(0) + "가 쓰러졌습니다.");
+											break outerLoop;
+										}
 										System.out.println(fire[i] + "의 " + skill_dic.get((4 * j) + s));
 										break;
 									}
@@ -311,7 +303,7 @@ public class pokemon_game {
 								continue;
 							}
 						} else if (use.equals("2") | use.equals("회복약")) {
-							if(my_hp == inven_hp.get(0)) {
+							if (my_hp == inven_hp.get(0)) {
 								System.out.println("최대 체력입니다.");
 							}
 							if (my_hp < (inven_hp.get(0))) {
@@ -328,16 +320,21 @@ public class pokemon_game {
 							System.out.println("교체할 포켓몬이 없습니다.");
 							continue;
 						} else if (count > 0) {
-							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까?(1,2,3,4,5)");
+							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까? (1~" + inven_mon.size() + ")");
 							int change = sc.nextInt();
-							Collections.swap(inven_mon, 0, change - 1);
-							Collections.swap(inven_hp, 0, change - 1);
-							Collections.swap(inven_skill, 0, 4 * (change - 1));
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
-							my_hp = inven_hp.get(0);
-							continue;
+							if (change == 1) {
+								System.out.println("똑같은 포켓몬으로 교체할 수 없습니다.");
+								continue;
+							} else if (change >= 2) {
+								Collections.swap(inven_mon, 0, change - 1);
+								Collections.swap(inven_hp, 0, change - 1);
+								Collections.swap(inven_skill, 0, 4 * (change - 1));
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
+								my_hp = inven_hp.get(0);
+								continue;
+							}
 						}
 						break;
 					}
@@ -380,6 +377,10 @@ public class pokemon_game {
 											System.out.println(snow[i] + "를 처치했습니다.");
 											break outerLoop;
 										}
+										if (my_hp <= 0) {
+											System.out.println(inven_mon.get(0) + "가 쓰러졌습니다.");
+											break outerLoop;
+										}
 										System.out.println(snow[i] + "의 " + skill_dic.get((4 * j) + s));
 										break;
 									}
@@ -413,7 +414,7 @@ public class pokemon_game {
 								continue;
 							}
 						} else if (use.equals("2") | use.equals("회복약")) {
-							if(my_hp == inven_hp.get(0)) {
+							if (my_hp == inven_hp.get(0)) {
 								System.out.println("최대 체력입니다.");
 							}
 							if (my_hp < (inven_hp.get(0))) {
@@ -430,16 +431,21 @@ public class pokemon_game {
 							System.out.println("교체할 포켓몬이 없습니다.");
 							continue;
 						} else if (count > 0) {
-							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까?(1,2,3,4,5)");
+							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까? (1~" + inven_mon.size() + ")");
 							int change = sc.nextInt();
-							Collections.swap(inven_mon, 0, change - 1);
-							Collections.swap(inven_hp, 0, change - 1);
-							Collections.swap(inven_skill, 0, 4 * (change - 1));
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
-							my_hp = inven_hp.get(0);
-							continue;
+							if (change == 1) {
+								System.out.println("똑같은 포켓몬으로 교체할 수 없습니다.");
+								continue;
+							} else if (change >= 2) {
+								Collections.swap(inven_mon, 0, change - 1);
+								Collections.swap(inven_hp, 0, change - 1);
+								Collections.swap(inven_skill, 0, 4 * (change - 1));
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
+								my_hp = inven_hp.get(0);
+								continue;
+							}
 						}
 						break;
 					}
@@ -482,6 +488,10 @@ public class pokemon_game {
 											System.out.println(water[i] + "를 처치했습니다.");
 											break outerLoop;
 										}
+										if (my_hp <= 0) {
+											System.out.println(inven_mon.get(0) + "가 쓰러졌습니다.");
+											break outerLoop;
+										}
 										System.out.println(water[i] + "의 " + skill_dic.get((4 * j) + s));
 										break;
 									}
@@ -515,7 +525,7 @@ public class pokemon_game {
 								continue;
 							}
 						} else if (use.equals("2") | use.equals("회복약")) {
-							if(my_hp == inven_hp.get(0)) {
+							if (my_hp == inven_hp.get(0)) {
 								System.out.println("최대 체력입니다.");
 							}
 							if (my_hp < (inven_hp.get(0))) {
@@ -532,16 +542,21 @@ public class pokemon_game {
 							System.out.println("교체할 포켓몬이 없습니다.");
 							continue;
 						} else if (count > 0) {
-							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까?(1,2,3,4,5)");
+							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까? (1~" + inven_mon.size() + ")");
 							int change = sc.nextInt();
-							Collections.swap(inven_mon, 0, change - 1);
-							Collections.swap(inven_hp, 0, change - 1);
-							Collections.swap(inven_skill, 0, 4 * (change - 1));
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
-							my_hp = inven_hp.get(0);
-							continue;
+							if (change == 1) {
+								System.out.println("똑같은 포켓몬으로 교체할 수 없습니다.");
+								continue;
+							} else if (change >= 2) {
+								Collections.swap(inven_mon, 0, change - 1);
+								Collections.swap(inven_hp, 0, change - 1);
+								Collections.swap(inven_skill, 0, 4 * (change - 1));
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
+								my_hp = inven_hp.get(0);
+								continue;
+							}
 						}
 						break;
 					}
@@ -549,9 +564,10 @@ public class pokemon_game {
 			}
 
 			if (where.equals("BOSS")) {
-				if (count < 3) {
-					System.out.println("BOSS에 도전할 자격은 포켓몬 3마리 이상부터 입니다.");
-				} else if (count >= 3) {
+				if (count < 5) {
+					System.out.println("BOSS에 도전할 자격은 포켓몬 5마리 이상부터 입니다.");
+					System.out.println("현재 포유 포켓몬: " + inven_mon);
+				} else if (count >= 5) {
 					System.out.println(boss + "가 나타났다!!");
 					System.out.println("가랏, " + inven_mon.get(0) + "!!!\n");
 					my_hp = inven_hp.get(0);
@@ -577,7 +593,48 @@ public class pokemon_game {
 									my_hp -= boss_skillp[s];
 									if (boss_hp <= 0) {
 										System.out.println(boss + "를 처치했습니다.");
-										break outerLoop;
+										System.out.println("choi 박사: "+name+" 덕분에 세계평화가 찾아왔네^^ ");
+										System.out.println("이제 발 뻗고 잘구 있겠구만 ㅎㅎ 고맙네!!!");
+										System.out.println("---------------------------------");
+										System.out.println("        ⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⠀ ⠀⠀");
+										System.out.println("        ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⠀⠀");
+										System.out.println("        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿   ");
+										System.out.println("        ⢹⣿⣿⠟⠛⠛⠋⠛⠋⠛⠛⠟⠻⠿⣿⣿⣿⡿   ");
+										System.out.println("        ⢸⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⠇   ");
+										System.out.println("       ⢀⡘⣿⠇⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⠀   ");
+										System.out.println("       ⢸⣿⡏⣀⣉⡉⢛⢷⡄⠀⠀⢠⠶⠒⠒⠂⠈⡟⣄   ");
+										System.out.println("       ⠘⢿⡟⡅⠉⠉⠛⠛⡯⠠⠔⠀⠶⠶⠖⠦⣠⢿⠏   ");
+										System.out.println("        ⠸⣷⠑⠐⠀⠄⢠⠁⠀⠘⠀⠀⠀⠀⠠⠙⠡⠀ ⠀ ");
+										System.out.println("          ⠆⠀⠀⠀⠸⡀⠀⡀⠀⠀⠀⠀⠀⠁⠀⠀   ");
+										System.out.println("          ⠸⡄⠀⠀⢀⡀⠀⠀⠀⠀⠀⢀⠂⠀⠀⠀   ");
+										System.out.println("          ⣠⣿⡄⠀⠉⠅⠀⠈⠀⠀⡀⢉⠀⠀⠀    ");
+										System.out.println("       ⢀⠴⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⡸⡆⢄⠀⠀ ⠀  ");
+										System.out.println("    ⢀⣀⡴⡿⢿⣿⠹⣿⣿⣷⣶⡶⠂⠀⠀⠔⠀⡇⠀⠁⢁ ⠀ ");
+										System.out.println("⢀⣤⣴⣾⣿⢿⢞⡭⣵⣿⠀⠻⣿⣿⠟⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀  ");
+										System.out.println("⣿⣿⣿⣿⣟⣻⠪⣵⣿⣾⣷⢀⠀⠈⠣⢀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀ ");
+										System.out.println("⣿⣿⣿⣿⡷⢾⣯⢵⠖⠉⠈⡇⠀⠀⣰⠉⠷⣄⠀⠀⠀⣸⠁⠈⠂⢀ ");
+										System.out.println("--------------------------------");
+										System.out.println("만든이 : 이유찬, 천시영");
+										System.out.println("도움주신 분 : Google");
+										break outerLoop2;
+									}
+									if (my_hp <= 0) {
+										System.out.println(inven_mon.get(0) + "가 쓰러졌습니다.");
+										System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까? (1~" + inven_mon.size() + ")");
+										int change = sc.nextInt();
+										if (change == 1) {
+											System.out.println("똑같은 포켓몬으로 교체할 수 없습니다.");
+											continue;
+										} else if (change >= 2) {
+											Collections.swap(inven_mon, 0, change - 1);
+											Collections.swap(inven_hp, 0, change - 1);
+											Collections.swap(inven_skill, 0, 4 * (change - 1));
+											Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
+											Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
+											Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
+											my_hp = inven_hp.get(0);
+											continue;
+										}
 									}
 									System.out.println(boss + "의 " + boss_skill[s]);
 									break;
@@ -591,7 +648,7 @@ public class pokemon_game {
 							System.out.println("회복약을 사용하시겠습니까?(y/n)");
 							String use = sc.next();
 							if (use.equals("y")) {
-								if(my_hp == inven_hp.get(0)) {
+								if (my_hp == inven_hp.get(0)) {
 									System.out.println("최대 체력입니다.");
 								}
 								if (my_hp < (inven_hp.get(0))) {
@@ -606,16 +663,21 @@ public class pokemon_game {
 								continue;
 							}
 						} else if (choice.equals("포켓몬교체") | choice.equals("4")) {
-							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까?(1,2,3,4,5)");
+							System.out.print(inven_mon + "몇번째 포켓몬으로 교체하겠습니까? (1~" + inven_mon.size() + ")");
 							int change = sc.nextInt();
-							Collections.swap(inven_mon, 0, change - 1);
-							Collections.swap(inven_hp, 0, change - 1);
-							Collections.swap(inven_skill, 0, 4 * (change - 1));
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
-							Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
-							my_hp = inven_hp.get(0);
-							continue;
+							if (change == 1) {
+								System.out.println("똑같은 포켓몬으로 교체할 수 없습니다.");
+								continue;
+							} else if (change >= 2) {
+								Collections.swap(inven_mon, 0, change - 1);
+								Collections.swap(inven_hp, 0, change - 1);
+								Collections.swap(inven_skill, 0, 4 * (change - 1));
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 1);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 2);
+								Collections.swap(inven_skill, 1, 4 * (change - 1) + 3);
+								my_hp = inven_hp.get(0);
+								continue;
+							}
 						}
 					}
 				}
